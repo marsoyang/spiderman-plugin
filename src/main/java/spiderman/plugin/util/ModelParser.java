7f346988cbd37fe5d3ce41e6b92e69ebe4b66876
laiweiwei <l.weiwei@163.com>
2013-01-06 00:17:24 +0800

    fix bug and issue

--- src/main/java/spiderman/plugin/util/ModelParser.java
+++ src/main/java/spiderman/plugin/util/ModelParser.java
@@ -2,8 +2,8 @@ package spiderman.plugin.util;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.StringWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -24,7 +24,11 @@ import org.eweb4j.spiderman.xml.Field;
 import org.eweb4j.spiderman.xml.Target;
 import org.eweb4j.util.CommonUtil;
 import org.eweb4j.util.FileUtil;
+import org.eweb4j.util.xml.Attrs;
+import org.eweb4j.util.xml.Tags;
 import org.htmlcleaner.HtmlCleaner;
+import org.htmlcleaner.Serializer;
+import org.htmlcleaner.SimpleXmlSerializer;
 import org.htmlcleaner.TagNode;
 import org.htmlcleaner.XPatherException;
 import org.w3c.dom.Document;
@@ -36,11 +40,36 @@ import org.xml.sax.helpers.DefaultHandler;
 import com.greenpineyu.fel.FelEngine;
 import com.greenpineyu.fel.FelEngineImpl;
 import com.greenpineyu.fel.context.FelContext;
+import com.greenpineyu.fel.function.CommonFunction;
+import com.greenpineyu.fel.function.Function;
+
+//import com.greenpineyu.fel.FelEngine;
+//import com.greenpineyu.fel.FelEngineImpl;
+//import com.greenpineyu.fel.context.FelContext;
 
 public class ModelParser extends DefaultHandler{
 
 	private Target target = null;
 	private SpiderListener listener = null;
+	private static FelEngine fel = new FelEngineImpl();
+	static {
+        Function fun = new CommonFunction() {
+			public String getName() {
+				return "$output";
+			}
+
+			public Object call(Object[] arguments) {
+				Object node = arguments[0];
+				boolean keepHeader = false;
+				if (arguments.length > 2)
+					keepHeader = (Boolean) arguments[1];
+				
+				return ParserUtil.xml(node, keepHeader);
+			}
+		};
+		
+    	fel.addFun(fun);
+	}
 	public ModelParser(){}
 	public ModelParser(Target target, SpiderListener listener) {
 		this.target = target;
@@ -50,20 +79,42 @@ public class ModelParser extends DefaultHandler{
 	public static void main(String[] args) throws Exception{
 		File file = new File("d:\\xml.xml");
 		String xml = FileUtil.readFile(file);
-		 
 //		System.setProperty("javax.xml.xpath.XPathFactory:"+NamespaceConstant.OBJECT_MODEL_SAXON, "net.sf.saxon.xpath.XPathFactoryImpl");
-//		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-//        factory.setNamespaceAware(true); // never forget this!
-//        DocumentBuilder builder = factory.newDocumentBuilder();
-//        Document doc = builder.parse(new ByteArrayInputStream(xml.getBytes()));
-//        XPathFactory xfactory = XPathFactoryImpl.newInstance();
-//        XPath xpath = xfactory.newXPath();
-//        XPathExpression expr = xpath.compile("//node");
-//        Object result = expr.evaluate(doc, XPathConstants.NODESET);
-//        NodeList nodes = (NodeList) result;
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true); // never forget this!
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new ByteArrayInputStream(xml.getBytes()));
+        XPathFactory xfactory = XPathFactoryImpl.newInstance();
+        XPath xpath = xfactory.newXPath();
+        XPathExpression expr = xpath.compile("//node");
+        Object result = expr.evaluate(doc, XPathConstants.NODESET);
+        NodeList nodes = (NodeList) result;
+        
 //        int count = 0;
 //        String regex = "\\w+\\.(gif|png|jpg|jpeg|bmp)";
-//        for (int i = 0; i < nodes.getLength(); i++) {
+        for (int i = 0; i < nodes.getLength(); i++) {
+        	if (i > 0)
+        		break;
+            
+        	NodeList subs = (NodeList)xpath.compile("Description").evaluate(nodes.item(i), XPathConstants.NODESET);
+        	
+        	Node node = subs.item(0);
+        	FelContext ctx = fel.getContext();
+        	ctx.set("$this", node);
+        	Tags $Tags = Tags.me();
+        	Attrs $Attrs = Attrs.me();
+			ctx.set("$Tags", $Tags);
+			ctx.set("$Attrs", $Attrs);
+    		
+			System.out.println($Attrs.xml(ParserUtil.xml(node, false)).rm("style").Tags().kp("p").ok());
+    		
+    		System.out.println(fel.eval("$Attrs.xml($output($this)).rm('style').Tags().kp('p').ok()"));
+    		
+//    		Object newVal =  MVEL.eval("org.eweb4j.util.CommonUtil.toXml($this, false)", ctx);
+//    		System.out.println(newVal);
+        }
+
+//        	
 //            NodeList subs = (NodeList)xpath.compile("*[matches(text(),'"+regex+"')]/text()").evaluate(nodes.item(i), XPathConstants.NODESET);
 //            if (subs == null || subs.getLength() == 0)
 //            	continue;
@@ -76,6 +127,15 @@ public class ModelParser extends DefaultHandler{
 //            }
 //        }
 //        System.out.println("count->"+count);
+        
+        String html = "<div id='desc'><p><a href='http://www.baidu.com'>click me</a></p></div>";
+        HtmlCleaner cleaner = new HtmlCleaner();
+		TagNode tagNode = cleaner.clean(html);
+		Object[] nodeVals = tagNode.evaluateXPath("//p");
+		StringWriter sw = new StringWriter();  
+	    Serializer serializer = new SimpleXmlSerializer(cleaner.getProperties());  
+	    serializer.write((TagNode)nodeVals[0], sw, "UTF-8");
+//	    System.out.println(sw.getBuffer().toString());
 		
 //		//第一步：获得解析工厂的实例  
 //        SAXParserFactory spf = SAXParserFactory.newInstance();  
@@ -101,22 +161,22 @@ public class ModelParser extends DefaultHandler{
 //	        sp.parse(in, handler);
 //	        
 //	        return handler.getMap();
-			return parseXml(page.getContentData());
+			return parseXml(page);
 		}
 		
 		// TODO 解析 JSON
 		
 		//解析html
 		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-		list.add(parseHtml(page.getContent()));
+		list.add(parseHtml(page));
 		return list;
 	}
 
-	private List<Map<String, Object>> parseXml(byte[] xml) throws Exception{
+	private List<Map<String, Object>> parseXml(Page page) throws Exception{
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         factory.setNamespaceAware(true); // never forget this!
         DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.parse(new ByteArrayInputStream(xml));
+        Document doc = builder.parse(new ByteArrayInputStream(page.getContentData()));
         XPathFactory xfactory = XPathFactoryImpl.newInstance();
         XPath xpathParser = xfactory.newXPath();
         
@@ -141,6 +201,7 @@ public class ModelParser extends DefaultHandler{
 		}
 		return list;
 	}
+	
 	private Map<String, Object> parse2Map(Object item, XPath xpathParser, final List<Field> fields) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		for (Field field : fields){
@@ -150,55 +211,56 @@ public class ModelParser extends DefaultHandler{
 				String attribute = field.getParser().getAttribute();
 				String regex = field.getParser().getRegex();
 				String isArray = field.getIsArray();
-				String el = field.getParser().getEl();
+				String exp = field.getParser().getExp();
 				
 				XPathExpression expr = xpathParser.compile(xpath);
 		        Object result = expr.evaluate(item, XPathConstants.NODESET);
 		        
-//		        listener.onInfo(Thread.currentThread(), "xpath -> " + xpath + " parse result -> " + result);
 				if (result == null)
 					continue;
 				NodeList nodes = (NodeList) result;
 				if (nodes.getLength() == 0)
 					continue;
 				
-				Collection<Object> value = new ArrayList<Object>();
+				Collection<Object> values = new ArrayList<Object>();
+				
 				if (attribute != null && attribute.trim().length() > 0){
-					for (int i = 0; i < nodes.getLength(); i++) {
+					for (int i = 0; i < nodes.getLength(); i++){
 						Node node = nodes.item(i);
 						Element e = (Element)node;
-						value.add(e.getAttribute(attribute));
+						String attrVal = e.getAttribute(attribute);
+						values.add(attrVal);
 					}
-				}else {
-					for (int i = 0; i < nodes.getLength(); i++) {
+					
+					//正则
+					parseByRegex(regex, values);
+					// EXP表达式
+					parseByExp(exp, values);
+				}else if (xpath.endsWith("/text()")){
+					for (int i = 0; i < nodes.getLength(); i++){
 						Node node = nodes.item(i);
-						value.add(node.getNodeValue());
+						values.add(node.getNodeValue());
 					}
-				}
-				
-				parseByRegex(regex, value);
-				
-				// 进行EL表达式的解析
-				if (el != null && el.trim().length() > 0){
-					List<Object> newValue = new ArrayList<Object>();
-					for (Object val : value){
-						FelEngine fel = new FelEngineImpl();   
-						FelContext ctx = fel.getContext();
-						ctx.set("$text", val);
-						Object newVal = fel.eval(el);
-						if (newVal == null)
-							newVal = val;
-						
-						newValue.add(newVal);
+					//正则
+					parseByRegex(regex, values);
+					// EXP表达式
+					parseByExp(exp, values);
+				} else {
+					for (int i = 0; i < nodes.getLength(); i++){
+						Node node = nodes.item(i);
+						values.add(node);
 					}
-					value.clear();
-					value.addAll(newValue);
+					// 此种方式获取到的Node节点大部分都不是字符串，因此先执行表达式后执行正则
+					// EXP表达式
+					parseByExp(exp, values);
+					//正则
+					parseByRegex(regex, values);
 				}
 				
 				if ("1".equals(isArray))
-					map.put(key, value);
+					map.put(key, values);
 				else
-					map.put(key, new ArrayList<Object>(value).get(0));
+					map.put(key, new ArrayList<Object>(values).get(0));
 			} catch (Exception e) {
 				listener.onError(Thread.currentThread(), e.toString(), e);
 				continue;
@@ -208,11 +270,11 @@ public class ModelParser extends DefaultHandler{
 		return map;
 	}
 	
-	private Map<String, Object> parseHtml(String html){
+	private Map<String, Object> parseHtml(Page page){
 		Map<String, Object> map = new HashMap<String, Object>();
 		final List<Field> fields = target.getModel().getField();
 		HtmlCleaner cleaner = new HtmlCleaner();
-		TagNode rootNode = cleaner.clean(html);
+		TagNode rootNode = cleaner.clean(page.getContent());
 		
 		for (Field field : fields){
 			try {
@@ -221,25 +283,50 @@ public class ModelParser extends DefaultHandler{
 				String attribute = field.getParser().getAttribute();
 				String regex = field.getParser().getRegex();
 				String isArray = field.getIsArray();
+				String exp = field.getParser().getExp();
+				
 				Object[] nodeVals = rootNode.evaluateXPath(xpath);
-//				listener.onInfo(Thread.currentThread(), "xpath -> " + xpath + " parse result -> " + CommonUtil.toJson(nodeVals));
 				if (nodeVals == null || nodeVals.length == 0)
 					continue;
 				
-				Collection<Object> value = new ArrayList<Object>();
+				Collection<Object> values = new ArrayList<Object>();
+				
 				if (attribute != null && attribute.trim().length() > 0){
 					for (Object nodeVal : nodeVals){
 						TagNode node = (TagNode)nodeVal;
-						value.add(node.getAttributeByName(attribute));
+						String attrVal = node.getAttributeByName(attribute);
+						values.add(attrVal);
 					}
-				}else 
-					value.addAll(Arrays.asList(nodeVals));
+					//正则
+					parseByRegex(regex, values);
+					// EXP表达式
+					parseByExp(exp, values);
+				}else if (xpath.endsWith("/text()")){
+					for (Object nodeVal : nodeVals){
+						values.add(nodeVal.toString());
+					}
+					//正则
+					parseByRegex(regex, values);
+					// EXP表达式
+					parseByExp(exp, values);
+				} else {
+					for (Object nodeVal : nodeVals){
+						TagNode node = (TagNode)nodeVal;
+						values.add(node);
+					}
+					// 此种方式获取到的Node节点大部分都不是字符串，因此先执行表达式后执行正则
+					// EXP表达式
+					parseByExp(exp, values);
+					//正则
+					parseByRegex(regex, values);
+				}
 				
-				this.parseByRegex(regex, value);
+				
+				
 				if ("1".equals(isArray))
-					map.put(key, value);
+					map.put(key, values);
 				else
-					map.put(key, new ArrayList<Object>(value).get(0).toString());
+					map.put(key, new ArrayList<Object>(values).get(0).toString());
 				
 			} catch (XPatherException e) {
 				listener.onError(Thread.currentThread(), e.toString(), e);
@@ -249,22 +336,44 @@ public class ModelParser extends DefaultHandler{
 		
 		return map;
 	}
-	private void parseByRegex(String regex, Collection<Object> value) {
-		if (regex != null && regex.trim().length() > 0){
-			Collection<String> inputs = new ArrayList<String>(value.size());
-			for (Object obj : value){
-				String input = String.valueOf(obj);
-				List<String> vals = CommonUtil.findByRegex(input, regex);
-				if (vals != null)
-					inputs.addAll(vals);
-				else
-					inputs.add(input);
-			}
+	private void parseByExp(String exp, Collection<Object> list) {
+		if (exp == null || exp.trim().length() == 0)
+			return ;
 			
-			if (!inputs.isEmpty()){
-				value.clear();
-				value.addAll(inputs);
-			}
+		List<Object> newValue = new ArrayList<Object>();
+		for (Object val : list){
+			fel.getContext().set("$this", val);
+			Tags $Tags = Tags.me();
+        	Attrs $Attrs = Attrs.me();
+        	fel.getContext().set("$Tags", $Tags);
+        	fel.getContext().set("$Attrs", $Attrs);
+    		Object newVal = fel.eval(exp);
+			if (newVal == null)
+				newVal = val;
+			
+			newValue.add(newVal);
+		}
+		
+		list.clear();
+		list.addAll(newValue);
+	}
+	
+	private void parseByRegex(String regex, Collection<Object> list) {
+		if (regex == null || regex.trim().length() == 0)
+			return ;
+		List<Object> newVals = new ArrayList<Object>(list.size());
+		for (Object obj : list) {
+			String input = (String)obj;
+			List<String> vals = CommonUtil.findByRegex(input, regex);
+			if (vals == null)
+				newVals.add(obj);
+			else
+				newVals.addAll(vals);
+		}
+		
+		if (!newVals.isEmpty()){
+			list.clear();
+			list.addAll(newVals);
 		}
 	}
 }
